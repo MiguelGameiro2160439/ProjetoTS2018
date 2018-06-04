a51cc76a9842512110946252b6d7c23a092594ea
21703330 <2170330@my.ipleiria.pt>
2018-06-04 09:31:04 +0100

    aaaaa

---
+++ TS-Cliente/TS-Servidor/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TS_Servidor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
